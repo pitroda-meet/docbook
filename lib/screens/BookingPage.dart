1996300589dce020acd15ab73b93445b38a8ff61
pitroda-meet <mpitroda459@rku.ac.in>
2024-10-30 18:12:57 +0530

    CAN USER BOOK DOCTOR APOINTMENT

--- lib/screens/BookingPage.dart
+++ lib/screens/BookingPage.dart
@@ -1,14 +1,17 @@
-import 'package:docbook/screens/AppointmentScreen.dart';
-import 'package:docbook/screens/bottom_bar_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'AppointmentScreen.dart';
+import 'package:docbook/screens/bottom_bar_widget.dart';
 
 class BookingPage extends StatefulWidget {
+  final String doctorId; // Add doctorId here
   final String doctorName;
   final String specialization;
   final String imagePath;
 
   const BookingPage({
     super.key,
+    required this.doctorId, // Include doctorId in the constructor
     required this.doctorName,
     required this.specialization,
     required this.imagePath,
@@ -19,12 +22,12 @@ class BookingPage extends StatefulWidget {
 }
 
 class _BookingPageState extends State<BookingPage> {
-  int _currentIndex = 0; // State to keep track of the selected index
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _ageController = TextEditingController();
+  final TextEditingController _mobileController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   String _selectedGender = 'Male';
-  final DateTime _selectedDay = DateTime.now();
-  final TimeOfDay _selectedTime = const TimeOfDay(hour: 14, minute: 0);
 
   @override
   Widget build(BuildContext context) {
@@ -35,165 +38,140 @@ class _BookingPageState extends State<BookingPage> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        // Add SingleChildScrollView to prevent pixel overflow and allow scrolling
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Display doctor information
               Row(
                 children: [
                   CircleAvatar(
                     radius: 40,
-                    backgroundImage: AssetImage(widget.imagePath),
+                    backgroundImage:
+                        NetworkImage(widget.imagePath), // Use NetworkImage
                   ),
                   const SizedBox(width: 16),
                   Expanded(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          widget.doctorName,
-                          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          widget.specialization,
-                          style: const TextStyle(color: Colors.grey),
-                        ),
+                        Text(widget.doctorName,
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(widget.specialization,
+                            style: const TextStyle(color: Colors.grey)),
                       ],
                     ),
                   ),
                 ],
               ),
               const SizedBox(height: 20),
-
-              // Patient's Name
               const Text('Patient\'s Name'),
-              TextField(
+              TextFormField(
                 controller: _nameController,
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your name' : null,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 10),
-
-              // Age Section
               const Text('Age'),
-              TextField(
+              TextFormField(
                 controller: _ageController,
+                keyboardType: TextInputType.number,
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your age' : null,
                 decoration: const InputDecoration(
                   hintText: 'Enter Age',
                   border: OutlineInputBorder(),
                 ),
-                keyboardType: TextInputType.number,
               ),
               const SizedBox(height: 10),
-
-              // Gender Selection
               const Text('Gender'),
               Row(
                 children: [
-                  Row(
-                    children: [
-                      Radio(
-                        value: 'Male',
-                        groupValue: _selectedGender,
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedGender = value.toString();
-                          });
-                        },
-                      ),
-                      const Text('Male'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Radio(
-                        value: 'Female',
-                        groupValue: _selectedGender,
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedGender = value.toString();
-                          });
-                        },
-                      ),
-                      const Text('Female'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Radio(
-                        value: 'Others',
-                        groupValue: _selectedGender,
-                        onChanged: (value) {
-                          setState(() {
-                            _selectedGender = value.toString();
-                          });
-                        },
-                      ),
-                      const Text('Others'),
-                    ],
-                  ),
+                  _buildGenderRadio('Male'),
+                  _buildGenderRadio('Female'),
+                  _buildGenderRadio('Others'),
                 ],
               ),
               const SizedBox(height: 10),
-
-              // Mobile Number
               const Text('Mobile Number'),
-              const TextField(
-                decoration: InputDecoration(
+              TextFormField(
+                controller: _mobileController,
+                keyboardType: TextInputType.phone,
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your mobile number' : null,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 10),
-
-              // Email
               const Text('Email'),
-              const TextField(
-                decoration: InputDecoration(
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) =>
+                    value!.isEmpty ? 'Please enter your email' : null,
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 20),
-
-              // Confirm Booking Button
               ElevatedButton(
                 onPressed: () {
-                  // Navigate to AppointmentScreen and pass selected data
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AppointmentScreen(
-                        doctorName: widget.doctorName,
-                        specialization: widget.specialization,
-                        selectedDate: _selectedDay,
-                        selectedTime: _selectedTime,
-                        patientName: _nameController.text,
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AppointmentScreen(
+                          doctorId: widget.doctorId,
+                          doctorName: widget.doctorName,
+                          specialization: widget.specialization,
+                          patientName: _nameController.text,
+                          patientAge: _ageController.text,
+                          patientGender: _selectedGender,
+                          patientMobile: _mobileController.text,
+                          patientEmail: _emailController.text,
+                        ),
                       ),
-                    ),
-                  );
+                    );
+                  }
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.teal,
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 ),
                 child: const Text('Confirm Booking'),
               ),
-              const SizedBox(height: 35), // Add spacing to avoid bottom navigation overlap
             ],
           ),
         ),
       ),
       bottomNavigationBar: BottomBarWidget(
-        currentIndex: _currentIndex,
-        onTabTapped: (index) {
-          setState(() {
-            _currentIndex = index; // Update the current index when a tab is selected
-          });
-        },
+        currentIndex: 0,
+        onTabTapped: (int value) {},
       ),
+    );
+  }
+
+  Widget _buildGenderRadio(String gender) {
+    return Row(
+      children: [
+        Radio(
+          value: gender,
+          groupValue: _selectedGender,
+          onChanged: (value) {
+            setState(() {
+              _selectedGender = value.toString();
+            });
+          },
+        ),
+        Text(gender),
+      ],
     );
   }
 }
